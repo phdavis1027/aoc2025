fa4397acd0a6e480a1248722e7f643f0d7171ed3
Phillip Davis <phdavis1027@gmail.com>
2025-12-06 13:47:47 -0500

    wip: day 5 part 2
    I'm very confident that my algorithm is correct given a correct
    implementation of the delete operation, but that operation is a real
    bear it seems,

--- common/src/main/kotlin/RangeTree.kt
+++ common/src/main/kotlin/RangeTree.kt
@@ -6,12 +6,13 @@ data class Interval(val low: Long, val high: Long) : Comparable<Interval> {
 			throw IndexOutOfBoundsException()
 		}
 
-		if (n == low) return Pair(null, this)
-		if (n == high) return Pair(this, null)
+		if (n == low) return Pair(null, Interval(low + 1, high))
+		if (n == high) return Pair(Interval(low, high - 1), null)
 
+		// n is strictly between low and high
 		return Pair(
-			Interval(low, n),
-			Interval(n, high)
+			Interval(low, n - 1),
+			Interval(n + 1, high)
 		)
 	}
 }
@@ -20,6 +21,20 @@ fun Long.overlaps(interval: Interval): Boolean = this >= interval.low && this <=
 
 class RangeTree {
 	var root: Node? = null
+
+	fun bubble(node: Node): Node {
+		var left = node.left
+		var right = node.right
+		assert(left != null)
+		assert(right != null)
+		assert(left!!.color == Color.BLACK)
+		assert(right!!.color == Color.BLACK)
+		assert(node.color == Color.BLACK)
+		left.color = Color.BLACK
+		right.color = Color.BLACK
+		node.color = Color.RED
+		return node
+	}
 
 	// Returns the inserted node
 	fun insert(interval: Interval): Node {
@@ -54,17 +69,39 @@ class RangeTree {
 		return this.fixup(z)
 	}
 
+	// Returns the node which was made red
+	private fun swapColor(x: Node, y: Node): Node {
+		assert(x.color != y.color)
+		if (x.color == Color.RED) {
+			y.color = Color.RED
+			x.color = Color.BLACK
+			return y
+		} else {
+			y.color = Color.BLACK
+			x.color = Color.RED
+			return x
+		}
+	}
+
 	private fun fixup(z_: Node): Node {
 		fun RangeTree.case3Right(z: Node) {
-			z.parent!!.color = Color.BLACK
-			z.parent!!.parent!!.color = Color.RED
-			this.rightRotate(z.parent!!.parent!!)
+			this.rightRotate(
+				this.swapColor(
+					z.parent!!,
+					z.parent!!.parent!!
+				)
+			)
 		}
+
 		fun RangeTree.case3Left(z: Node) {
-			z.parent!!.color = Color.BLACK
-			z.parent!!.parent!!.color = Color.RED
-			this.leftRotate(z.parent!!.parent!!)
+			this.leftRotate(
+				this.swapColor(
+					z.parent!!,
+					z.parent!!.parent!!
+				)
+			)
 		}
+
 		var z = z_
 		if (z.parent == null) {
 			assert(this.root == z)
@@ -90,10 +127,7 @@ class RangeTree {
 				var y = z.parent!!.parent!!.right
 				if (y.color() == Color.RED) {
 					// y is not null, since null.color() == Color.BLACK
-					z.parent!!.color = Color.BLACK
-					y!!.color = Color.BLACK
-					z.parent!!.parent!!.color = Color.RED
-					z = z.parent!!.parent!!
+					z = this.bubble(z.parent!!.parent!!)
 				} else if (z == z.parent!!.right) {
 					z = z.parent!!
 					this.leftRotate(z)
@@ -105,10 +139,7 @@ class RangeTree {
 				var y = z.parent!!.parent!!.left
 				if (y.color() == Color.RED) {
 					// y is not null, since null.color() == Color.BLACK
-					z.parent!!.color = Color.BLACK
-					y!!.color = Color.BLACK
-					z.parent!!.parent!!.color = Color.RED
-					z = z.parent!!.parent!!
+					z = this.bubble(z.parent!!.parent!!)
 				} else if (z == z.parent!!.left) {
 					z = z.parent!!
 					this.rightRotate(z)
@@ -148,7 +179,9 @@ class RangeTree {
 		} else if (x == x.parent!!.left) {
 			// Tell x's parent about their new child
 			x.parent!!.left = y
-		} else { x.parent!!.right = y }
+		} else {
+			x.parent!!.right = y
+		}
 		y.left = x
 		x.parent = y
 
@@ -178,8 +211,10 @@ class RangeTree {
 		} else if (y == y.parent!!.left) { // Is x a left child?
 			// Tell x's parent about their new child
 			y.parent!!.left = x
-		} else { y.parent!!.right = x }
-		x.right= y
+		} else {
+			y.parent!!.right = x
+		}
+		x.right = y
 		y.parent = x
 
 		return x
@@ -213,7 +248,8 @@ class RangeTree {
 		var parent: Node? = null,
 		var color: Color
 	) {
-
+		fun isLeftChild() = this == this.parent!!.left
+		fun isRightChild() = this == this.parent!!.right
 
 		companion object {
 			fun withoutChildren(interval: Interval): Node {
@@ -222,10 +258,129 @@ class RangeTree {
 		}
 	}
 
-	enum class Color {
-		RED,
-		BLACK
+	enum class Color(i: Int) {
+		RED(0),
+		BLACK(1)
 	}
+
+	fun delete(z: Node) {
+		var y = z
+		var yOriginalColor = y.color
+		var x: Node? = z.left
+		if (z.left == null) {
+			x = z.right
+			this.transplant(z, x!!)
+		} else if (z.right == null) {
+			x = z.left
+			this.transplant(z, x!!)
+		} else {
+			// SAFETY: We just established that both
+			// children are non-null
+			y = this.minimum(z.right!!)
+			yOriginalColor = y.color
+			x = y.right
+			if (y.parent == z) {
+				// TODO: SAFETY: How do we know that y.right is not null?
+				x!!.parent = y
+			} else {
+				this.transplant(y, y.right!!)
+				y.right = z.right
+				y.right!!.parent = y
+			}
+			this.transplant(z, y)
+			y.left = z.left
+			z.left!!.parent = y
+			y.color = z.color
+		}
+		if (yOriginalColor == Color.BLACK) {
+			deleteFixup(x!!)
+		}
+	}
+
+	fun minimum(node: Node): Node {
+		var answer: Node = node
+		while (answer.left != null) {
+			answer = answer.left!!
+		}
+		return answer
+	}
+
+	private fun deleteFixup(x_: Node) {
+		var x: Node? = x_
+		fun case3Left(w: Node) {
+			w.color = x!!.parent!!.color
+			x!!.parent!!.color = Color.BLACK
+			w.right!!.color = Color.BLACK
+			this.leftRotate(x!!.parent!!)
+			x = this.root
+		}
+
+		fun case3Right(w: Node) {
+			w.color = x!!.parent!!.color
+			x!!.parent!!.color = Color.BLACK
+			w.left!!.color = Color.BLACK
+			this.rightRotate(x!!.parent!!)
+			x = this.root
+		}
+		while (x != this.root && x?.color == Color.BLACK) {
+			if (x!!.isLeftChild()) {
+				var w = x?.parent?.right
+				if (w!!.color == Color.RED) {
+					w.color = Color.BLACK
+					x!!.parent!!.color = Color.RED
+					this.leftRotate(x!!.parent!!)
+				}
+				if (w.left!!.color == Color.BLACK && w.right!!.color == Color.BLACK) {
+					w.color = Color.RED
+					x = x!!.parent
+				} else if (w.right?.color == Color.BLACK) {
+					w.left!!.color = Color.BLACK
+					w.color = Color.RED
+					this.rightRotate(w)
+					w = x?.parent?.right
+					case3Left(w!!)
+				} else {
+					case3Left(w)
+				}
+			} else {
+				var w = x?.parent?.right
+				if (w!!.color == Color.RED) {
+					w.color = Color.BLACK
+					x!!.parent!!.color = Color.RED
+					this.rightRotate(x!!.parent!!)
+				}
+				if (w.right!!.color == Color.BLACK && w.left!!.color == Color.BLACK) {
+					w.color = Color.RED
+					x = x!!.parent
+				} else if (w.left?.color == Color.BLACK) {
+					w.right!!.color = Color.BLACK
+					w.color = Color.RED
+					this.leftRotate(w)
+					w = x?.parent?.left
+					case3Right(w!!)
+				} else {
+					case3Right(w)
+				}
+			}
+		}
+		x!!.color = Color.BLACK
+	}
+
+	private fun transplant(u: Node, v: Node) {
+		// SAFETY: `u.isLeftChild` and `u.parent!!.right`
+		// are both safe to call because of this check
+		// plus the fact that this is all single-threaded
+		if (u.parent == null) {
+			this.root = v
+		} else if (u.isLeftChild()) {
+			u.parent = v
+		} else {
+			u.parent!!.right = v
+		}
+		v.parent = u.parent
+	}
+
+	fun peek(): Interval? = this.root?.interval
 
 	fun Node?.color(): Color = this?.color ?: Color.BLACK
 	fun Node?.setColor(color: Color) {
--- day05/src/main/kotlin/Day05.kt
+++ day05/src/main/kotlin/Day05.kt
@@ -1,8 +1,10 @@
-fun RangeTree.overlapsAny(value: Long): Boolean {
-	fun check(node: RangeTree.Node?): Boolean {
-		if (node == null) return false
-		if (value.overlaps(node.interval)) return true
-		return check(node.left) || check(node.right)
+fun RangeTree.overlapsAny(value: Long): RangeTree.Node? {
+	fun check(node: RangeTree.Node?): RangeTree.Node? {
+		if (node == null) return null
+		if (value.overlaps(node.interval)) return node
+		val left = check(node.left)
+		if (left != null) return left
+		return check(node.right)
 	}
 	return check(root)
 }
@@ -18,16 +20,50 @@ fun partOne(input: List<String>) {
 		tree.insert(Interval(low, high))
 	}
 
-	val count = valueLines.map { it.toLong() }.count { tree.overlapsAny(it) }
+	val count = valueLines.map { it.toLong() }.count { tree.overlapsAny(it) != null }
 	println("Answer: $count")
 }
 
 fun partTwo(input: List<String>) {
-	println("Answer: TODO")
+	val blankIndex = input.indexOf("")
+	val intervalLines = input.subList(0, blankIndex)
+
+	val tree = RangeTree.empty()
+	for (line in intervalLines) {
+		val (low, high) = line.split("-").map { it.toLong() }
+		tree.insert(Interval(low, high))
+	}
+
+	var answer = 0
+	// Each iteration of the root counts exactly one valid ID
+	while (tree.root != null) {
+		++answer
+		val interval = tree.peek()
+		// SAFETY: interval is only null if tree.root is null,
+		// so we can safely assert it
+		val n = interval!!.low
+		// This loop removes all elements of `tree`
+		// which contain `n`, but leaves all other integers
+		// contained in exactly the same number of intervals.
+		while (true) {
+			val r = tree.overlapsAny(n) ?: break
+			val (lo, hi) = interval.splitAt(n)
+			tree.delete(r)
+			if (lo != null) {
+				tree.insert(lo)
+			}
+			if (hi != null) {
+				tree.insert(hi)
+			}
+		}
+	}
+
+	println("Answer: $answer valid IDs")
 }
 
 object TestCasePartOne {
-	@JvmStatic fun main(args: Array<String>) =
+	@JvmStatic
+	fun main(args: Array<String>) =
 		partOne(
 			java.io.File("src/test.txt")
 				.readText()
@@ -37,7 +73,8 @@ object TestCasePartOne {
 }
 
 object SolvePartOne {
-	@JvmStatic fun main(args: Array<String>) =
+	@JvmStatic
+	fun main(args: Array<String>) =
 		partOne(
 			java.io.File("src/input.txt")
 				.readText()
@@ -47,7 +84,8 @@ object SolvePartOne {
 }
 
 object TestCasePartTwo {
-	@JvmStatic fun main(args: Array<String>) =
+	@JvmStatic
+	fun main(args: Array<String>) =
 		partTwo(
 			java.io.File("src/test.txt")
 				.readText()
@@ -57,7 +95,8 @@ object TestCasePartTwo {
 }
 
 object SolvePartTwo {
-	@JvmStatic fun main(args: Array<String>) =
+	@JvmStatic
+	fun main(args: Array<String>) =
 		partTwo(
 			java.io.File("src/input.txt")
 				.readText()
